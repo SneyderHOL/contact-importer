43e0cf807eb60cf03c4173cf991b58f6a42cfe5a
SneyderHOL <1507@holbertonschool.com>
2021-04-08 10:29:39 -0500

    Add tests for users and factory setup

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,5 +1,6 @@
 FactoryBot.define do
   factory :user do
-    
+    sequence(:email) { |n| "username#{n}@example.com" } #email { "username@example.com" }
+    password { "password" }
   end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,41 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe '#validations' do
+    let(:user) { build(:user) }
+    it 'test that factory object is valid' do
+      expect(user).to be_valid
+    end
+
+    it 'test that factory email sequence is valid' do
+      user.save
+      second_user = build(:user)
+      expect(second_user).to be_valid
+    end
+
+    it 'test that user object has no email' do
+      user.email = ''
+      expect(user).not_to be_valid
+      expect(user.errors[:email]).to include("can't be blank")
+    end
+
+    it 'test that user object has no valid email' do
+      aggregate_failures do
+        user.email = 'abcd'
+        expect(user).not_to be_valid
+        expect(user.errors[:email]).to include("is invalid")
+
+        user.email = 'user@'
+        expect(user).not_to be_valid
+        expect(user.errors[:email]).to include("is invalid")
+      end
+    end
+
+    it 'test that user object has repeated email' do
+      user.save
+      invalid_user = build(:user, email: user.email)
+      expect(invalid_user).not_to be_valid
+      expect(invalid_user.errors[:email]).to include("has already been taken")
+    end
+  end
 end

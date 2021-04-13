c06aecd5cf9efdb5031869e65cb90eeebfb37f79
SneyderHOL <1507@holbertonschool.com>
2021-04-13 11:53:54 -0500

    Update contacts test factory

--- spec/factories/contacts.rb
+++ spec/factories/contacts.rb
@@ -4,7 +4,7 @@ FactoryBot.define do
     birthdate { "20210408" }
     phone { "(+57) 320 432 05 09" }
     address { "99 Street #55-55 Main Avenue" }
-    credit_card { "371449635398431" }
+    credit_card { "***********8431" }
     franchise { "American Express" }
     sequence(:email) { |n| "contact#{n}@example.com" }
     user { nil }

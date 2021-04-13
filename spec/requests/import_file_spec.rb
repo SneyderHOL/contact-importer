c7afed9c384e6c0a296ae4213f02c1501dfda1bd
SneyderHOL <1507@holbertonschool.com>
2021-04-13 09:00:18 -0500

    Update tests

--- spec/requests/import_file_spec.rb
+++ spec/requests/import_file_spec.rb
@@ -1,7 +1,42 @@
 require 'rails_helper'
 
 RSpec.describe "ImportFiles", type: :request do
-  describe "GET /index" do
-    pending "add some examples (or delete) #{__FILE__}"
+  let(:user) { create(:user) }
+  #file = fixture_file_upload('files/users-2021-03-26.csv', 'text/csv')
+  describe "GET /import" do
+    it 'redirect to the log_in page as a non logged in user' do
+      get '/import'
+      expect(response).to redirect_to(new_user_session_path)
+      follow_redirect!
+      expect(response.body).to include("Log in")
+    end
+
+    it 'returns a success response as a logged in user' do
+      aggregate_failures do
+        sign_in user
+        get root_path
+        expect(response).to be_successful
+        get '/import'
+        expect(response).to be_successful
+        expect(response.body).to include("Import Contacts")
+      end
+    end
   end
+
+  # describe "POST /import" do
+  #   it 'returns a success response as a logged in user' do
+  #     aggregate_failures do
+  #       sign_in user
+  #       post '/import'
+  #       expect(response).to redirect_to(root_path)
+  #       follow_redirect!
+  #       expect(response.body).to include("Log in")
+  #       # get root_path
+  #       # expect(response).to be_successful
+  #       # get '/import'
+  #       # expect(response).to be_successful
+  #       # expect(response.body).to include("Import Contacts")
+  #     end
+  #   end
+  # end
 end

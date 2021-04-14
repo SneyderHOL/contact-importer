076e7272d86ab1d1f3fc6d0a6a73894fb55456c7
SneyderHOL <1507@holbertonschool.com>
2021-04-14 09:18:42 -0500

    Update user logic

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -1,2 +1,7 @@
 class UserController < ApplicationController
+  def imported_files
+    @user = current_user
+    @user.import_files
+    @imported_files = ImportFile.paginate(page: params[:page], per_page: 10)
+  end
 end

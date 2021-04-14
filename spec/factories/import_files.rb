8fdd73a747730c8baea4b16c01c0cfc500183558
SneyderHOL <1507@holbertonschool.com>
2021-04-14 09:11:22 -0500

    Add more tests cases

--- spec/factories/import_files.rb
+++ spec/factories/import_files.rb
@@ -3,8 +3,8 @@ FactoryBot.define do
     status { "on hold" }
     user { nil }
     after (:build) do |import_file|
-      import_file.file.attach(io: File.open(Rails.root.join('tmp', 'storage', 'users-2021-03-26.csv')),
-      filename: 'users-2021-03-26.csv', content_type: 'text/csv')
+      import_file.file.attach(io: File.open(Rails.root.join('tmp', 'storage', 'testing_file.csv')),
+      filename: 'testing_file.csv', content_type: 'text/csv')
     end
   end
 end
--- spec/models/import_file_spec.rb
+++ spec/models/import_file_spec.rb
@@ -1,9 +1,9 @@
 require 'rails_helper'
 
 RSpec.describe ImportFile, type: :model do
+  let(:user) { create(:user) }
+  let(:import_file) { build(:import_file, user: user) }
   describe '#validations' do
-    let(:user) { create(:user) }
-    let(:import_file) { build(:import_file, user: user) }
     it 'test that is invalid empty fields' do
       aggregate_failures do
         import_file.status = ''
@@ -52,6 +52,153 @@ RSpec.describe ImportFile, type: :model do
       import_file.file.attach(io: File.open(Rails.root.join('tmp', 'storage', 'gatsby.png')),
                               filename: 'gatsby.png')
       expect(import_file).not_to be_valid
+    end
+  end
+
+  describe '#processing' do
+    it 'test that correctly process a csv file' do
+      aggregate_failures do
+        import_file.column = { "name" => "name", "birthdate" => "birthdate", "phone" => "phone",
+          "address" => "address", "credit_card" => "credit_card", "email" => "email" }
+        import_file.save
+        expect(import_file.status).to eq("on hold")
+        expect(FailedRegister.all.count).to eq(0)
+        expect(Contact.all.count).to eq(0)
+        import_file.processing
+        expect(import_file.status).to eq("finished")
+        expect(FailedRegister.all.count).to eq(0)
+        expect(Contact.all.count).to eq(8)
+      end
+    end
+  end
+
+  describe '#private methods' do
+    it 'test that stores a failed register' do
+      aggregate_failures do
+        expect(FailedRegister.all.count).to eq(0)
+        import_file.set_failed_register("Birthdate wrong date format")
+        expect(FailedRegister.all.count).to eq(1)
+      end
+    end
+
+    it 'test that do not stores a failed register' do
+      aggregate_failures do
+        expect(FailedRegister.all.count).to eq(0)
+        import_file.set_failed_register("")
+        expect(FailedRegister.all.count).to eq(0)
+      end
+    end
+
+    it 'test true in has_valid_headers?' do
+      aggregate_failures do
+        import_file.column = { "name" => "name", "birthdate" => "birthdate", "phone" => "phone",
+                             "address" => "address", "credit_card" => "credit_card", "email" => "email" }
+        import_file.save
+        headers = ["name", "email", "address", "phone", "credit_card", "birthdate"]
+        expect(import_file.has_valid_headers?(headers)).to eq(true)
+      end
+    end
+
+    it 'test false in has_valid_headers?' do
+      aggregate_failures do
+        import_file.column = { "name" => "nombre", "birthdate" => "fecha", "phone" => "tele",
+                             "address" => "dir", "credit_card" => "tarjeta", "email" => "correo" }
+        import_file.save
+        headers = ["name", "email", "address", "phone", "credit_card", "birthdate"]
+        expect(import_file.has_valid_headers?(headers)).to eq(false)
+      end
+    end
+
+    it 'test return a franchise key in hash' do
+      aggregate_failures do
+        attributes = { name: "James Bond", birthdate: "19750202", phone: "(+07) 123 123 12 12",
+                      address: "some street", credit_card: "4111111111111111", "email" => "james@example.com" }
+        expect(import_file.get_franchise(attributes)).to eq({ franchise: "Visa" })
+        attributes[:credit_card] = "371449635398431"
+        expect(import_file.get_franchise(attributes)).to eq({ franchise: "American Express" })
+        attributes[:credit_card] = "6011111111111117"
+        expect(import_file.get_franchise(attributes)).to eq({ franchise: "Discover" })
+        attributes[:credit_card] = "30569309025904"
+        expect(import_file.get_franchise(attributes)).to eq({ franchise: "Diners Club" })
+        attributes[:credit_card] = "3530111333300000"
+        expect(import_file.get_franchise(attributes)).to eq({ franchise: "JCB" })
+        attributes[:credit_card] = "5555555555554444"
+        expect(import_file.get_franchise(attributes)).to eq({ franchise: "MasterCard" })
+      end
+    end
+
+    it 'test return a franchise nil in hash' do
+      aggregate_failures do
+        attributes = { name: "James Bond", birthdate: "19750202", phone: "(+07) 123 123 12 12",
+                      address: "some street", credit_card: "4411111111111111", "email" => "james@example.com" }
+        expect(import_file.get_franchise(attributes)).to eq({ franchise: nil })
+      end
+    end
+
+    it 'test return an encrypted credit_card string' do
+      aggregate_failures do
+        credit_card = "4111111111111111"
+        expect(import_file.encrypt_cc(credit_card)).to eq("************1111")
+      end
+    end
+
+    it 'test return a hash with contacts attributes' do
+      aggregate_failures do
+        import_file.column = { "name" => "nombre", "birthdate" => "fecha", "phone" => "tele",
+                              "address" => "dir", "credit_card" => "tarjeta", "email" => "correo" }
+        row = { "nombre" => "James Bond", "fecha" => "19750202", "tele" => "(+07) 123 123 12 12",
+                "dir" => "some street", "tarjeta" => "371449635398431", "correo" => "james@example.com" }
+        result = { name: "James Bond", birthdate: "19750202", phone: "(+07) 123 123 12 12",
+                  address: "some street", credit_card: "***********8431",
+                  email: "james@example.com", franchise: "American Express" }
+        expect(import_file.get_contact_attributes(row)).to eq(result)
+      end
+    end
+
+    it 'test that return a "finished" ImportFile status after reading a csv' do
+      aggregate_failures do
+        import_file.column = { "name" => "name", "birthdate" => "birthdate", "phone" => "phone",
+          "address" => "address", "credit_card" => "credit_card", "email" => "email" }
+        import_file.save
+        expect(FailedRegister.all.count).to eq(0)
+        expect(Contact.all.count).to eq(0)
+        result = import_file.read_csv_file
+        expect(FailedRegister.all.count).to eq(0)
+        expect(Contact.all.count).to eq(8)
+        expect(result).to eq({ status: "finished"})
+      end
+    end
+
+    it 'test that return a "failed" ImportFile status after reading a csv by bad headers' do
+      aggregate_failures do
+        import_file.file.attach(io: File.open(Rails.root.join('tmp', 'storage', 'bad_headers.csv')),
+                                filename: 'bad_headers.csv', content_type: 'text/csv')
+        import_file.column = { "name" => "name", "birthdate" => "birthdate", "phone" => "phone",
+          "address" => "address", "credit_card" => "credit_card", "email" => "email" }
+        import_file.save
+        expect(FailedRegister.all.count).to eq(0)
+        expect(Contact.all.count).to eq(0)
+        result = import_file.read_csv_file
+        expect(FailedRegister.all.count).to eq(1)
+        expect(Contact.all.count).to eq(0)
+        expect(result).to eq({ status: "failed"})
+      end
+    end
+
+    it 'test that return a "failed" ImportFile status after reading a csv by bad data' do
+      aggregate_failures do
+        import_file.file.attach(io: File.open(Rails.root.join('tmp', 'storage', 'bad_data.csv')),
+                                filename: 'bad_data.csv', content_type: 'text/csv')
+        import_file.column = { "name" => "name", "birthdate" => "birthdate", "phone" => "phone",
+          "address" => "address", "credit_card" => "credit_card", "email" => "email" }
+        import_file.save
+        expect(FailedRegister.all.count).to eq(0)
+        expect(Contact.all.count).to eq(0)
+        result = import_file.read_csv_file
+        expect(FailedRegister.all.count).to eq(8)
+        expect(Contact.all.count).to eq(0)
+        expect(result).to eq({ status: "failed"})
+      end
     end
   end
 end

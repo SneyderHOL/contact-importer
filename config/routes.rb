e72399139aeab3b78424e4adaab866ea1983223c
SneyderHOL <1507@holbertonschool.com>
2021-04-14 09:19:04 -0500

    Update routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
   root 'welcome#index'
+  get 'user/imported_files', to: 'user#imported_files'
+  get 'import', to: 'import_file#import'
+  post 'import', to: 'import_file#upload'
+  resources :contacts, only: [:index]
+  #get 'failed_registers', to: ''
 end
